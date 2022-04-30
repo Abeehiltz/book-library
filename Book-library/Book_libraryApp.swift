80e46d48a529c12af5b97a67597d5e138cafe7a8
Abeehiltz <abeehiltz@icloud.com>
2022-04-30 23:37:27 +0200

    Initial Commit

---
+++ Book-library/Book_libraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_libraryApp.swift
+//  Book-library
+//
+//  Created by Elizabeth Bricout on 2022-04-30.
+//
+
+import SwiftUI
+
+@main
+struct Book_libraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
